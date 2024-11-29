91a313d9e4765e4636eb9e8d69cffd828c93ab9f
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-11-29 17:43:14 +0545

    this is

---
+++ operator.py
@@ -0,0 +1,34 @@
+#arithmetic operator
+# + - / *
+
+#logical operator
+#AND OR NOT
+
+#assignment operator
+# =
+
+#comparison operator
+# < > <= >=
+
+#bitwise operator
+# = ||
+
+
+#WAP Program addition , subtraction , multiplication of two any number
+
+firstNumber =30
+secondNumber =40
+
+sum=firstNumber+secondNumber
+sub = firstNumber-secondNumber
+mul = firstNumber*secondNumber
+div = secondNumber/secondNumber
+
+
+print(f"Sum of {firstNumber} number and {secondNumber} is:" ,sum, "------", end=" ")
+print("Subtraction of two number is",sub)
+print("Multiplication of two number is ",mul)
+print(div)
+
+
+#what are the use of end in print ("hello ", end =" ,")
--- typeConversion.py
+++ typeConversion.py
@@ -1,7 +1,11 @@
+#type of data conversion [syntax:class_name(variable)]
 name = "55" 
 age ="55"
 ageSecond =56.4
 
-result= int(name)
 
+#print-data
+#print_dataType_ type()
+
+result= int(name)
 print(type(result))
