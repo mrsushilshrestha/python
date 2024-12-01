f2ae59d8ffae52fa9fee9e3f636d7fd949cd55dc
sushil <shresthasushil6341@gmail.com>
2024-12-01 15:22:40 +0545

    this

---
+++ input-type.py
@@ -0,0 +1,8 @@
+print("Enter the Two number:")
+
+firstNumber = int(input())
+secondNumber = int(input())
+
+sum = firstNumber + secondNumber
+
+print(f"The Sum of {firstNumber} and {secondNumber} is: ", sum)
