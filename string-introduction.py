bca8d3c2d4efe100491c707c8674b4cf60d31d03
sushil <shresthasushil6341@gmail.com>
2024-12-01 15:35:20 +0545

    add somthing

---
+++ string-introduction.py
@@ -0,0 +1,17 @@
+#introduction of string in python
+str1= "Hello Sushil"
+str2 = 'Hello sushil'
+str3 = """Hello sushil"""
+
+print(str1)
+print(str2)
+print(str3)
+
+#if we need to print somthing like " " '' in display we use diffrent "" '' """  """ in our code
+str4= "Hello what's up ";
+str5 = 'Hello this is "021710" My symbol Number'
+str6=  """Hello this is "021710" My symbol Number. Hi what's up"""
+
+print(str4)
+print(str5)
+print(str6)
