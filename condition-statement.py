77e994fc37d19e6d2cbd27e4f2d6561c87d483dc
sushil <shresthasushil6341@gmail.com>
2024-12-01 23:43:08 +0545

    Python Note update

---
+++ condition-statement.py
@@ -0,0 +1,9 @@
+print("Enter Your age for Voting:")
+age=int(input())
+
+if(age>18):
+    print("You can Vote")
+elif(age==18):
+    print("Become the Adult")
+else:
+    print("Sorry! You can Not Vote At The Time")
---
+++ condition-statement1.py
@@ -0,0 +1,19 @@
+print("Enter the Day, you want to check:")
+day=input()
+
+if(day=="sunday"):
+    print("Wou Today is Holiday!")
+elif(day=="monday"):
+    print("Sorry! No any Day Off")
+elif(day=="tuesday"):
+    print("Sorry! No any Day Off")
+elif(day=="wednesday"):
+    print("Sorry! No any Day Off")
+elif(day=="Thursday"):
+    print("Sorry! No any Day Off")
+elif(day=="friday"):
+    print("Sorry! No any Day Off")
+elif(day=="saturday"):
+    print("Sorry! No any Day Off")
+else:
+    print("Sorry! Please Check the Day. This is Not exiting.")
