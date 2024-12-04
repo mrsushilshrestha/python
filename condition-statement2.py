50ca12067731c574f2654ee2c93dd3fc542fa5a1
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-04 12:09:01 +0545

    Make Result Using Condition

--- condition-statement2.py
+++ condition-statement2.py
@@ -1,16 +1,21 @@
 print("Enter the marks of the student:")
-marks =float(input())
+marks =input()
 
-if(marks>=90):
-    grade="A"
-elif(marks<90 and marks>=80 ):
-    grade="B"
-elif(marks<80 and marks>=70):
-    grade="C"
-elif(marks<70 and marks>=32):
-    grade="D"
-else:
-    grade="F"
-
-
-print("The Grade Obtain By Student is:",grade)
+if not marks.isdigit():
+    print("INVALID! Please Enter Number Only")
+    
+else: 
+    marks=int(marks) 
+       
+    if(marks>=90):
+        grade="A"
+    elif(marks<90 and marks>=80 ):
+        grade="B"
+    elif(marks<80 and marks>=70):
+        grade="C"
+    elif(marks<70 and marks>=32):
+        grade="D"
+    else:
+        grade="F"
+    
+    print("The Grade Obtain By Student is:",grade)
