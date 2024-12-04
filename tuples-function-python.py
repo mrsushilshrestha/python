9a1c0e29d806bfbc5cf1cf913f4755983cb3f68d
sushil <shresthasushil6341@gmail.com>
2024-12-04 14:29:32 +0545

    Tupple Method In Python

---
+++ tuples-function-python.py
@@ -0,0 +1,9 @@
+#Tuple Method Or Function In Python
+
+tup = (1,2,4,3,5,2,3,2,2,)
+
+#.index(element)
+print(tup.index(4))  #return the idex of first occurrence # return 2
+
+#.count(element)
+print(tup.count(2)) #Count the Total Number of Occurrence occur . 
