ef2f410a421a9ff783e5420520c2ca2bce86b703
sushil <shresthasushil6341@gmail.com>
2024-12-04 14:20:40 +0545

    Tuples In Python Update

---
+++ tuples-python.py
@@ -0,0 +1,16 @@
+#Tupple is a immutable
+#tup =(1,2,3,4)
+
+tupple_values =(1,2,4,3,5)
+
+print(type(tupple_values))
+print(tupple_values[0])
+print(tupple_values[4])
+
+#To create a single Value tuple
+tupple_values =(1,)
+print(type(tupple_values))
+print(tupple_values)
+
+
+
