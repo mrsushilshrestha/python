f7cd3411de5269adfa83963104276a7d79968f2b
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-09 23:24:10 +0545

    for loop

---
+++ for-loop.py
@@ -0,0 +1,11 @@
+name = "tribhuvanuniversity"
+for i in range(18, -1, -1):#reverse string
+    print(name[i],end=',')
+
+
+
+for i in range (6,1,-1):
+    for j in range (i):
+        print("*",end='')
+    print(" ")
+
