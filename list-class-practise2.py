67b3f82bf137f76892d034a0be4102ab9038f203
sushil <shresthasushil6341@gmail.com>
2024-12-10 22:41:54 +0545

    class practise

--- list-class-practise2.py
+++ list-class-practise2.py
@@ -6,15 +6,25 @@ list_first.append(input("Enter the address: "))
 
 print(list_first)
 
-
 """
-number=int(input("Enter the number of people"))
+#WAP to take a input from the user and make different list for different user.
+number=int(input("Enter the number of people: "))
 i=1
-list_second=[]
+list_third=[]
+
 while i<=number:
-    list_second.append(input("Enter the Name:"))
-    list_second.append(input("Enter the Age:"))
-    list_second.append(input("Enter the Address:"))      
+    list_second=[]
+    print(f"For user {i}")
+    list_second.append(input(f"Enter the Name : "))
+    list_second.append(input(f"Enter the Age: "))
+    list_second.append(input(f"Enter the Address ")) 
+
+    list_third.append(list_second)
     i+=1
-    
-print(list_second)
+
+print(list_third)
+
+
+
+
+
