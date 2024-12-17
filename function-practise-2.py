d1ba85b170a05ba0a73bec99e8f3f74f9f8aadb3
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-17 08:52:45 +0545

    Updatae Caluclator

--- function-practise-2.py
+++ function-practise-2.py
@@ -1,5 +1,4 @@
 #Making Calculator using functions
-
 def add(number_first,number_second):
     print(number_first+number_second)
 
@@ -15,18 +14,26 @@ def div(number_first ,number_second):
     else:
         print("Number NOT Divided By Zero")
 
-
+message ="""
+Enter the Option What you want!!!!
+1>ADDITION
+2>SUBTRACTION
+3>MULTIPLICATION
+4>DIVISION
+"""
+print(message)
+operator =input("Enter the Operator or Options[+,-,*,/]: ")
 first_number =int(input("Enter the first Number: "))
-operator =input("Enter the Operator[+,-,*,/]: ")
 second_number =int(input("Enter the Second Number: "))
 
-if operator=='+':
+if operator=='+' or 1:
     add(first_number,second_number)
-elif operator=='-':
+elif operator=='-' or 2:
     sub(first_number,second_number)
-elif operator=='*':
+elif operator=='*' or 3:
     mul(first_number,second_number)
-elif operator=='/':
+elif operator=='/' or 4:
     div(first_number,second_number)
 else:
     print("INVALID!!!")
+
