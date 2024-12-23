5fb55ae5877bf41fbc8557098d9315ca0cd974b1
sushil <shresthasushil6341@gmail.com>
2024-12-24 00:08:23 +0545

    update

---
+++ oops/3-oops_example_practise.py
@@ -0,0 +1,42 @@
+# class employee:
+    
+#     def __init__(self,first_number,second_number,operator):
+#        print("Function Running...")
+#        self.first_number = first_number
+#        self.second_number = second_number
+#        self.operator=operator
+
+#     def perform(self):
+#            if operator =="+":
+#                return self.sum()
+           
+#            elif operator == "-":
+#                return self.sub()
+           
+#            elif operator == "*":
+#                return self.mul()
+           
+#            elif operator == "/":
+#                return self.div()
+#            else:
+#                print("Invalide Options!!!")
+       
+#     def sum(self):
+#         return first_number+second_number
+#     def sub(self):
+#         return first_number-second_number             
+
+
+
+# first_number =int(input("Enter the first Number"))
+# second_number = int (input("Enter the Second Number "))
+# operator = input("Enter the operator")
+
+    
+
+# obj =employee(first_number,second_number,operator)
+
+# result=obj.perform()
+# print(result)
+
+
