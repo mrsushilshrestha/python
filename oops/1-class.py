86e9ce6b2e8923c2217b3e2e85d9252fc6e46f11
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-23 17:00:00 +0545

    Update

---
+++ oops/1-class.py
@@ -0,0 +1,17 @@
+class Person:
+    country ="Nepal"
+    gender ="make"
+    
+class Animal:
+    family ="Dog"
+        
+
+
+p1 =Person()
+ani = Animal()
+
+
+print(p1.country)
+print(p1.gender)
+
+print(ani.family)
---
+++ oops/2-calculator.py
@@ -0,0 +1,32 @@
+class Calculator:
+    output_sentance =" of two number is :"
+    
+    def sum(self,a,b): #function for additions
+        # a=5
+        # b=9
+        print("Addition"+self.output_sentance,a+b)
+    
+    def sub(self,a,b): #function for subtraction
+        # a=5
+        # b=9
+         print("Addition"+self.output_sentance,a-b)
+        
+    
+    def mul(self,a,b): #function for multiplication
+        # a=5
+        # b=9
+        print("Addition"+self.output_sentance,a*b)
+    
+    
+    def div(self,a,b): #function for division
+        # a=5
+        # b=9
+        print("Addition"+self.output_sentance,a/b)
+        
+
+c1= Calculator()
+c1.sum(5,10)
+c1.sub(10,5)
+c1.mul(10,5)
+c1.div(10,5)
+
