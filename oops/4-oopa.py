d67152159f0432582b91b86816a15aea10180d72
sushil <shresthasushil6341@gmail.com>
2024-12-23 21:34:34 +0545

    update

--- oops/4-oopa.py
+++ oops/4-oopa.py
@@ -1,15 +1,25 @@
-dictionary_employee={}
-list_employee=[]
-new_salary=[]
-for i in range(3):
-    dictionary_employee["name"]=input("Enter the Name of employee: ")
-    dictionary_employee["salary"]=input("Enter the Salary of employee: ")
-    dictionary_employee["bonus"]=input("Enter the Bonus of employee: ")
-    
-    list_employee.append(dictionary_employee)
+list_employee=[
+    {"name":"sushil","salary":"18000","bonus":"10"},
+    {"name":"ram","salary":"19000","bonus":"30"},
+    {"name":"hari","salary":"28000","bonus":"20"}
+]
 
-for items in dictionary_employee:
-    print(items["salary"])
+# print(list_employee)
+def bonus_Calculation(salary,bonus):
+    bonus_amount= salary*bonus/100
+    net_salary = salary+ bonus_amount
+    return net_salary
+
+bonus_Calculation(1000,10)
+for items in list_employee:
+    name =items["name"]
+    salary =int(items["salary"])
+    bonus= int(items["bonus"])
+    
+    net_salary_employee=bonus_Calculation(salary,bonus)   
+
+    print(f"The salary of {name} is: {salary} and bonus is: {bonus} the net salary is: {net_salary_employee}")
+
 
     
 
