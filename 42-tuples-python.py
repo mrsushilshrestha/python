07455f3af073e653b92c9fbc994c41d4d39a6dd0
sushil <shresthasushil6341@gmail.com>
2024-12-26 00:43:57 +0545

    update

--- 42-tuples-python.py
+++ 42-tuples-python.py
@@ -1,9 +1,14 @@
+#Tupple In Python
 #Tupple is a immutable
+#A tuple is a collection which is ordered and unchangeable.
 #tup =(1,2,3,4)
+
+thistuple = ("apple", "banana", "cherry") #Create a Tuple
+print(thistuple)
 
 tupple_values =(1,2,4,3,5)
 
-print(type(tupple_values))
+print(type(tupple_values)) 
 print(tupple_values[0])
 print(tupple_values[4])
 
