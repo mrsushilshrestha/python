0ca3bb3a103f1f8a54ab6039c350388998faecda
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-28 13:34:29 +0545

    update New file

---
+++ 64-file-handling-practice.py
@@ -0,0 +1,12 @@
+#WAF that replace all occurrences of "java" with "python"in above file.
+
+with open("practice.txt","r") as f:
+    data=f.read()
+    # print(data)
+
+new_data=data.replace("Java","python")
+print(new_data)
+
+with open("practice.txt","w+") as f:
+    f.write(new_data)
+    
--- 64.1-file-handling-assignment.py
+++ 64.1-file-handling-assignment.py
@@ -1,1 +1,18 @@
-#hello world
+#Create a file "practice.txt" using python.Add the following data in it:
+"""
+    Hi everyone
+    we are learning File I/O
+    using Java.
+    I like programming In Java
+"""
+
+#to create a file
+with open("practice.txt","w")as f:
+    f.write( "Hi everyone\nwe are learning File I/O\nusing Java.\nI like programming In Java")
+    
+    
+with open("practice.txt","r")as f:
+    print(f.read())
+
+
+    
