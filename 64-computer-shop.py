6d140ace901629ab2b1785d531ad05cc26d0fdb8
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-28 21:46:35 +0545

    update

--- 64-computer-shop.py
+++ 64-computer-shop.py
@@ -1,7 +1,7 @@
 import os
 class Computer_shop:
 
-    def __init__(self,path):
+    def __init__(self,path): 
         self.path =path
 
     def write_function(self,name,brand,id): #wite function 
@@ -18,7 +18,7 @@ class Computer_shop:
     def read_function(self,word): #read function
         with open(self.path,"r")as file:
             data = True
-            line=1
+            # line=1
             while data:
                 data=file.readline()
                 if word in data:
@@ -29,24 +29,36 @@ class Computer_shop:
                     pass
             return print("Not Found!!!")     
 
-    def update_function(self,update_name,update_brand,update_id): #update function
+    def update_function(self,old_name,update_name): #update function
+        self.old_name = old_name
         self.update_name =update_name
-        self.update_brand =update_brand
-        self.update_id =update_id
 
-        with open(self.path,"a+")as file: #to update and Read data using the [a+] append mode
-            file.write(self.update_name+", ")
-            file.write(self.update_brand+", ")
-            file.write(self.update_id+", ")
-            file.write("\n")           
-
+        # with open(self.path,"a+")as file: #to update and Read data using the [a+] append mode
+        #     file.write(self.update_name+", ")
+        #     file.write(self.update_brand+", ")
+        #     file.write(self.update_id+", ")
+        #     file.write("\n")
+        
+        with open(self.path,"r") as f:
+            data =f.read()            
+            new_data=data.replace(self.old_name,self.update_name) 
+            # print(new_data) 
+        
+        with open(self.path,"w+") as f:
+            f.write(new_data)
+            
     #DANGER!!!
     def delete_function(self): #delete function
         # os.remove(path)
+        print("Sorry This Feature Not Available At The Moment!!! ")
         pass
 
 
-message=""""
+
+
+#---------value passing section------------------------------------------------------
+
+message="""
 Enter the Option
 1>Write Data
 2>Read Data
@@ -67,15 +79,14 @@ if option=='1': #Write data
     computer_shop_obj.write_function(name,brand,id)
 
 elif option=='2': #for read data
-    word=input("Enter the data To search: ")
+    word=input("Enter the Name to search: ")
     computer_shop_obj.read_function(word)
     
 elif option=='3': #for read data
-    print("Welcome Back! \nEnter the Data ")
-    update_name =input("Enter the Name Of Customer:- ")
-    update_brand =input("Enter the Brand Of Computer:- ")
-    update_id =input("Enter the Id of Customer:- ")
-    computer_shop_obj.update_function(update_name,update_brand,update_id)
+    print("You Click Update Option...")
+    old_name =input("Enter the Old Name Of Customer: ")
+    update_name =input("Enter the Name Of Customer: ")    
+    computer_shop_obj.update_function(old_name,update_name)
     
 elif option=='4': #for read data
     computer_shop_obj.delete_function()
