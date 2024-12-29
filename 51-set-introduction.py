bb5b22dcf2fce8d0e38d238ba4cfdc1bc393107a
sushil <shresthasushil6341@gmail.com>
2024-12-29 22:40:49 +0545

    update

--- 51-set-introduction.py
+++ 51-set-introduction.py
@@ -1,14 +1,17 @@
 # Set in Python
 # Set: Mutable, Unordered
-set_values1 ={"apple","mango","ball","cat"}
-set_values2 ={"dog","rabbit","apple","mango","graves"}
 
-set_result =set_values1.union(set_values2)
-print("union",set_result)
+set_values1 = {"apple", "mango", "ball", "cat"}
+set_values2 = {"dog", "rabbit", "apple", "mango", "graves"}
 
-set_result=set_values1.intersection(set_values2)
-print("Intersection",set_result)
+# Union: Combines all unique elements from both sets
+set_result = set_values1.union(set_values2)
+print("Union:", set_result)
 
-set_result=set_values1.difference(set_values2)
-print("Difference",set_result)
+# Intersection: Returns only the common elements between both sets
+set_result = set_values1.intersection(set_values2)
+print("Intersection:", set_result)
 
+# Difference: Returns elements present in set_values1 but not in set_values2
+set_result = set_values1.difference(set_values2)
+print("Difference:", set_result)
