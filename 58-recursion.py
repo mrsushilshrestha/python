4586bc9d50dbdbba5336753c3b21696318abd44a
sushil <shresthasushil6341@gmail.com>
2025-01-02 14:11:39 +0545

    update

--- 58-recursion.py
+++ 58-recursion.py
@@ -1,21 +1,21 @@
 #recursion Introduction
-# def display(num):
-#     print(num*num)
-#     if num<9:
-#         display(num+1)
+def display(num):
+    print(num*num)
+    if num<9:
+        display(num+1)
     
-# display(0)
+display(0)
     
 
-# # Recursion in Python
-# def display(n):
-#     if n==0:   #base case or control
-#         return 
+# Recursion in Python
+def display(n):
+    if n==0:   #base case or control
+        return 
 
-#     print(n)
-#     display(n-1)
+    print(n)
+    display(n-1)
 
-# display(5)
+display(5)
 
 # print number 1 2 3 4 5 6 7 8 9 10  using recursiom
 def recursion_function(num):
---
+++ python-my-calendar.py
@@ -0,0 +1,5 @@
+import calendar
+
+year= 2024
+month=12
+print(calendar.month(year,month))
