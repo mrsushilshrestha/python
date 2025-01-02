cde0d3c35f8401df82ad440df3a7ee6316305a88
sushil <shresthasushil6341@gmail.com>
2025-01-02 23:11:16 +0545

    update

--- scissors-paper-rock.py
+++ scissors-paper-rock.py
@@ -1,50 +1,51 @@
 # "scissors-paper-rock"
 import random
 
-class Tiktak:
+class RockPaperScissors:
     def __init__(self,user_input):
             self.user_input = user_input
             self.computer = random.choice(["scissors", "paper", "rock"])
             print(f"Your Input: {self.user_input}\nComputer Input: {self.computer}")
 
-    def game(self):
-        #   print(self.computer)
-        if self.user_input=='scissors' and self.computer=='paper':
-            print("You win!!!")    
-        elif self.user_input=='rock' and self.computer=='scissors':
-            print("You win!!!")       
-        elif self.user_input=='paper' and self.computer=='rock':
-            print("You win!!!") 
-        elif self.user_input==self.computer:       
-            print("Die Game!!!")
+
+    def play_game(self):
+        if self.user_input == self.computer:
+            print("It's a Tie!")
+        elif (self.user_input == 'scissors' and self.computer == 'paper') or \
+            (self.user_input == 'rock' and self.computer == 'scissors') or \
+            (self.user_input == 'paper' and self.computer == 'rock'):
+            print("You win!")
         else:
-            print("Computer win!!!") 
+            print("Computer wins!") 
 
-while True:
-    # main program
-    message ="""
-    1. Scissors
-    2. Paper
-    3. Rock
-    4. Exit
-    """
+def main():
+    while True:
+        # main program
+        message ="""
+        1. Scissors
+        2. Paper
+        3. Rock
+        4. Exit
+        """
 
-    user_input =input(message)
-    if user_input=='1':
-        user_input='scissors'
-    elif user_input=='2':
-        user_input='paper'
-    elif user_input=='3':
-        user_input='rock'
-    elif user_input=='4':
-        exit()
-    else:
-        print("Invalid option!!!")
-        
-        
+        user_input =input(message)
+        if user_input=='1':
+            user_input='scissors'
+        elif user_input=='2':
+            user_input='paper'
+        elif user_input=='3':
+            user_input='rock'
+        elif user_input == '4':
+            print("Exiting the game. Goodbye!")
+            break
+        else:
+            print("Invalid option! Please try again.")
+            continue
+            
+            
 
-    t =Tiktak(user_input)
-    t.game()
+        game =RockPaperScissors(user_input)
+        game.play_game()
 
 
-    
+main()
